73cb7c747bbbadc7c8bb33b6b276674856e2b346
Akash Sharma <akashsharma91795@gmail.com>
2025-09-13 19:06:21 +0530

    trying to fix background image problem

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <div className='flex flex-col items-center justify-center min-h-screen gap-5 bg-[url(./backGround.jpg)] bg-cover'>
+  <div className='flex flex-col items-center justify-center min-h-screen gap-5 bg-[url(../public/backGround.jpg)] bg-cover'>
     <App />
   </div>
   
